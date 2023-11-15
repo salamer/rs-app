555441062527812c78467c718be40ba4249b907c
salamer <salamer_gaga@163.com>
2023-11-16 00:38:49 +0800

    update: update

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+// use flate2::read::GzDecoder;
+// use std::io::prelude::*;
+// use std::io::Cursor;
+// use base64::{Engine as _, engine::general_purpose};
+// use std::io;
+
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
+
+// #[post("/echo")]
+// async fn echo(req_body: String) -> impl Responder {
+//     // let v = req_body.to_vec();
+//     println!("req_body {:?}", req_body);
+//     let bytes = general_purpose::STANDARD_NO_PAD.decode(req_body).unwrap();
+
+//     // let mut dr = GzDecoder::new(&*bytes);
+
+//     // let mut s = String::new();
+//     // dr.read_to_string(&mut s).unwrap();
+//     // println!("Decompressed: {:?}", s);
+
+//     let mut d = Vec::new();
+//     let mut rdr = snap::read::FrameDecoder::new(&bytes[..]);
+//     io::copy(&mut rdr, &mut d).unwrap();
+
+//     // let mut d = Vec::new();
+//     // match dr.read_to_end(&mut d) {
+//     //     Ok(_) => {
+//     //         println!("Decompressed: {:?}", std::str::from_utf8(&d).unwrap());
+//     //     }
+//     //     Err(e) => {
+//     //         println!("Error: {:?}", e);
+//     //     }
+//     // }
+//     // println!("Decompressed: {:?}", std::str::from_utf8(&d).unwrap());
+//     HttpResponse::Ok().body(())
+// }
+
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(hello)
+//             .service(echo)
+//             .route("/hey", web::get().to(manual_hello))
+//     })
+//     .bind(("127.0.0.1", 8080))?
+//     .run()
+//     .await
+// }
+
+use bytes::BytesMut;
+use http::{Request, Response};
+use std::convert::Infallible;
+use tower::{service_fn, Service, ServiceBuilder, ServiceExt};
+use tower_http::{compression::Compression, decompression::DecompressionLayer, BoxError};
+use std::io::stdout;
+use hyper::{body::HttpBody as _, Client};
+use tokio::io::{self, AsyncWriteExt as _};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+
+async fn fetch_url(url: hyper::Uri) -> Result<()> {
+    let client = Client::new();
+
+    let mut res = client.get(url).await?;
+
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    // Stream the body, writing each chunk to stdout as we get it
+    // (instead of buffering and printing at the end).
+
+    let mut data = Vec::new();
+
+    while let Some(next) = res.data().await {
+        let chunk = next?;
+        data.push(chunk);
+    }
+
+    println!("\n\nDone! {} bytes retrieved", data.len());
+
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url =  "http://localhost:8090/";
+
+    fetch_url(
+       url.parse::<hyper::Uri>().unwrap()
+    ).await
+}
---
+++ test-go/main.go
@@ -0,0 +1,74 @@
+// package main
+
+// import (
+// 	"encoding/base64"
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// 	"strings"
+
+// 	"github.com/golang/snappy"
+// )
+
+// func call() {
+// 	url := "http://127.0.0.1:8080/echo"
+
+// 	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+
+// 	encoded := snappy.Encode(nil, jsonStr)
+
+// 	sEnc := base64.RawStdEncoding.EncodeToString([]byte(encoded))
+
+// 	// req, _ := http.NewRequest("POST", url, bytes.NewBuffer(b.Bytes()))
+// 	req, _ := http.NewRequest("POST", url, strings.NewReader(sEnc))
+
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	fmt.Println("response Status:", resp.Status)
+// 	fmt.Println("response Headers:", resp.Header)
+// 	body, _ := io.ReadAll(resp.Body)
+// 	fmt.Println("response Body:", string(body))
+// }
+
+// func main() {
+// 	call()
+// }
+
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+func makeRandomString(size int) string {
+	var result []byte
+	for i := 0; i < size; i++ {
+		result = append(result, byte(65+rand.Intn(25)))
+	}
+	return string(result)
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK,
+			makeRandomString(10000),
+		)
+	})
+
+	// Listen and Server in 0.0.0.0:8080
+	if err := r.Run(":8090"); err != nil {
+		log.Fatal(err)
+	}
+}
